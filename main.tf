192af60390a177d82d13ad443f566dfa6e45a7f6
dongling.mq <dongling.chin@merquri.io>
2024-12-28 17:44:10 +0800

    subnet, internet gateway, route table

--- main.tf
+++ main.tf
@@ -11,3 +11,52 @@ resource "aws_vpc" "main" {
         Name = "main-vpc"
     }
 }
+
+# subnet A and B are created in the main VPC
+resource "aws_subnet" "A" {
+    vpc_id            = aws_vpc.main.id
+    cidr_block        = "10.0.1.0/24"
+    availability_zone = "us-east-1a"
+
+    tags = {
+        Name = "subnet A"
+    }
+}
+resource "aws_subnet" "B" {
+    vpc_id            = aws_vpc.main.id
+    cidr_block        = "10.0.2.0/24"
+    availability_zone = "us-east-1b"
+
+    tags = {
+        Name = "subnet B"
+    }
+}
+
+# Create an internet gateway
+resource "aws_internet_gateway" "main" {
+    vpc_id = aws_vpc.main.id
+
+    tags = {
+        Name = "main-igw"
+    }
+}
+
+# Create a route table for subnet A
+resource "aws_route_table" "A" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.main.id
+    }
+
+    tags = {
+        Name = "route-table-A"
+    }
+}
+
+# Associate the route table with subnet A
+resource "aws_route_table_association" "A" {
+    subnet_id      = aws_subnet.A.id
+    route_table_id = aws_route_table.A.id
+}
